d81c14df32e13d77f5cec407b6b0d8a684d562c5
Harsh Karki <corecorecrorecrore@gmail.com>
2025-12-02 21:29:08 +0530

    modify asset path with pulseboard prefix for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: process.env.NODE_ENV === 'production' ? '/pulseBoard/' : '/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
